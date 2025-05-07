d379176329ae3169d9f584f94ba5294497d096b7
lyoma <lyoma.abd@gmail.com>
2025-05-07 18:01:34 +0300

    add all

---
+++ index.js
@@ -0,0 +1,7 @@
+let userText = ('Введите текст')
+let  wordFromText = ('Введите слово из текста')
+let indexOfWord = (userText.trim().indexOf(wordFromText))
+
+prompt(userText)
+prompt (wordFromText)
+alert (indexOfWord)
