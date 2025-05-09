4bf4002f7a2f236c97babcb4a31ee83c8cd028ad
lyoma <lyoma.abd@gmail.com>
2025-05-09 15:36:06 +0300

    projectе

--- index.js
+++
@@ -1,7 +0,0 @@
-let userText = ('Введите текст')
-let  wordFromText = ('Введите слово из текста')
-let indexOfWord = (userText.trim().indexOf(wordFromText))
-
-prompt(userText)
-prompt (wordFromText)
-alert (indexOfWord)
